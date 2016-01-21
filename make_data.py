12f821cb0508b96a51a78c48d6610ce366a0c62d
aescobarr <agustiescobar@gmail.com>
2016-01-21 15:36:39 +0100

    Fixed typo in config keys

--- make_data.py
+++ make_data.py
@@ -7,11 +7,12 @@ import config
 
 this_year = datetime.now().year
 
-headers = {'Authorization': config.params[auth_token]}
+headers = {'Authorization': config.params['auth_token']}
+
 
 for year in range(2014, this_year+1):
     print str(year)
-    r = requests.get("http://" + config.params[server_url] + "/api/all_reports/?format=json" + "&year=" + str(year), headers=headers)
+    r = requests.get("http://" + config.params['server_url'] + "/api/all_reports/?format=json" + "&year=" + str(year), headers=headers)
     if r.status_code == 200:
         text_file = open("/home/webuser/webapps/tigaserver/static/all_reports" + str(year) + ".json", "w")
         text_file.write(r.text)
@@ -21,7 +22,7 @@ for year in range(2014, this_year+1):
         print 'Warning: report response status code for ' + str(year) + ' is ' + str(r.status_code)
 
 print('Starting coverage month request')
-r = requests.get("http://" + config.params[server_url]  + "/api/coverage_month/?format=json", headers=headers)
+r = requests.get("http://" + config.params['server_url']  + "/api/coverage_month/?format=json", headers=headers)
 if r.status_code == 200:
     text_file = open("/home/webuser/webapps/tigaserver/static/coverage_month_data.json", "w")
     text_file.write(r.text)
